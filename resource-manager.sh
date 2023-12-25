614d8a7dbbf6e381305a97c6e9959b85e30d8220
zakariaCHOUKRI <zc.zakariachoukri23@gmail.com>
2023-12-25 15:45:25 +0000

    Uncomment stuff

--- resource-manager.sh
+++ resource-manager.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# module load Python/3.8.6-GCCcore-10.2.0
-# pip install pandas
-# pip install dash
-# python3 main.py
+module load Python/3.8.6-GCCcore-10.2.0
+pip install pandas
+pip install dash
+python3 main.py
 
 # gpuspernode=1
 # cpuspernode=44
